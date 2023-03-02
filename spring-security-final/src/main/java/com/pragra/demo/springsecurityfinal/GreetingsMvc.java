be76b4fde4e880fef910b615846a3f6bfa2c3ff4
javajoy9 <neenuaghil@gmail.com>
2023-03-01 22:48:48 -0500

    Add Spring security demo final

---
+++ spring-security-final/src/main/java/com/pragra/demo/springsecurityfinal/GreetingsMvc.java
@@ -0,0 +1,18 @@
+package com.pragra.demo.springsecurityfinal;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class GreetingsMvc implements WebMvcConfigurer {
+
+
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/ca-en").setViewName("english");
+        registry.addViewController("/ca-fr").setViewName("french");
+    }
+}
---
+++ spring-security-final/src/test/java/com/pragra/demo/springsecurityfinal/SpringSecurityFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragra.demo.springsecurityfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
