ece09c00cb6d7588d538c9fb4aed75b31d7dc762
javajoy9 <neenuaghil@gmail.com>
2023-03-01 20:18:00 -0500

    add spring security demo initial

---
+++ spring-security-initial/src/main/java/com/pragra/demo/springsecurityinitial/SpringSecurityInitialApplication.java
@@ -0,0 +1,13 @@
+package com.pragra.demo.springsecurityinitial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInitialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInitialApplication.class, args);
+	}
+
+}
---
+++ spring-security-initial/src/test/java/com/pragra/demo/springsecurityinitial/SpringSecurityInitialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragra.demo.springsecurityinitial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInitialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
